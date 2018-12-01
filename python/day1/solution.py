abe16c512be0bd91c7e377c9dbdb8383bfb3130d
Kenny164 <peter.kinney@gmail.com>
2018-12-01 13:11:44 +0000

    adding day1-star2 structure

--- python/day1/solution.py
+++ python/day1/solution.py
@@ -1,4 +1,11 @@
-rollingTotal = 0
-for line in open("./inputs/day1.txt","r"):
-    rollingTotal += int(line)
-print(rollingTotal)
+def starOne():
+    rollingTotal = 0
+    for line in open("./inputs/day1.txt","r"):
+        rollingTotal += int(line)
+    return rollingTotal
+
+def starTwo():
+    pass
+
+if __name__ == "__main__":
+    print(starOne())
