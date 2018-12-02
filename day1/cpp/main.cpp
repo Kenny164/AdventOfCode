43d0ff927d581a37356adfcb515dd195fca32176
Kenny164 <peter.kinney@gmail.com>
2018-12-02 19:10:32 +0000

    tidied up the cpp a bit

--- day1/cpp/main.cpp
+++ day1/cpp/main.cpp
@@ -3,36 +3,28 @@
 #include <set>
 #include <vector>
 
-int starOne() {
-    std::ifstream fileIn ("inputs/day1.txt");
-    if (fileIn.is_open()) {
-        long rollingTotal = 0;
-        int current = 0;
-        while(fileIn >> current) {
-            rollingTotal += current;
-        }
-        return rollingTotal;
-    }
-    else {
+void extractInput(std::vector<int>& extractedInts, const char* filePath) {
+    std::ifstream fileIn (filePath);
+    if (!fileIn.is_open()) {
         std::cout << "File cannot open";
         exit(-2);
     }
+    int current = 0;
+    while(fileIn >> current) {
+        extractedInts.push_back(current);
+    }
+    fileIn.close();
 }
 
-int starTwo() {
-    std::ifstream fileIn ("inputs/day1.txt");
-    std::vector<int> extractedInts;
-    if (fileIn.is_open()) {
-        int current = 0;
-        while(fileIn >> current) {
-            extractedInts.push_back(current);
-        }
-        fileIn.close();
+int starOne(const std::vector<int>& extractedInts) {
+    int rollingTotal = 0;
+    for (std::vector<int>::size_type j=0; j<extractedInts.size(); j++) {
+        rollingTotal += extractedInts[j];
     }
-    else {
-        std::cout << "File cannot open";
-        exit(-2);
-    }
+    return rollingTotal;
+}
+
+int starTwo(const std::vector<int>& extractedInts) {
     std::set<int> historicTotal;
     int rollingTotal = 0;
     for (int i=0; i<10000; i++) {
@@ -49,7 +41,9 @@ int starTwo() {
 }
 
 int main(int argc, char const *argv[]) {
-    std::cout << starOne() << std::endl;
-    std::cout << starTwo() << std::endl;
+    std::vector<int> extractedInts;
+    extractInput(extractedInts, "inputs/day1.txt");
+    std::cout << starOne(extractedInts) << std::endl;
+    std::cout << starTwo(extractedInts) << std::endl;
     return 0;
 }
