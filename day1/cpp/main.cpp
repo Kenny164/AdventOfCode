1b82f0428c6786e53ed42106fef13b6a6ba48fe5
Kenny164 <peter.kinney@gmail.com>
2018-12-02 18:24:09 +0000

    cpp 1-1 working

--- day1/cpp/main.cpp
+++ day1/cpp/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <set>
+#include <vector>
 
 int starOne() {
     std::ifstream fileIn ("inputs/day1.txt");
@@ -18,7 +19,37 @@ int starOne() {
     }
 }
 
+int starTwo() {
+    std::ifstream fileIn ("inputs/day1.txt");
+    std::vector<int> extractedInts;
+    if (fileIn.is_open()) {
+        int current = 0;
+        while(fileIn >> current) {
+            extractedInts.push_back(current);
+        }
+        fileIn.close();
+    }
+    else {
+        std::cout << "File cannot open";
+        exit(-2);
+    }
+    std::set<int> historicTotal;
+    int rollingTotal = 0;
+    for (int i=0; i<10000; i++) {
+        for (std::vector<int>::size_type j=0; j<extractedInts.size(); j++) {
+            rollingTotal += extractedInts[j];
+            auto search = historicTotal.find(rollingTotal);
+            if (search != historicTotal.end()) {
+                return rollingTotal;
+            }
+            historicTotal.insert(rollingTotal);
+        }
+    }
+    return -1;
+}
+
 int main(int argc, char const *argv[]) {
-    std::cout << starOne();
+    std::cout << starOne() << std::endl;
+    std::cout << starTwo() << std::endl;
     return 0;
 }
