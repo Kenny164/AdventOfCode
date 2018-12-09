c95f27b16723290cab1d1bc576c7889f7ab5de13
Kenny164 <peter.kinney@gmail.com>
2018-12-09 22:17:26 +0000

    day3 star2 compled

--- day3/python/solution.py
+++ day3/python/solution.py
@@ -12,28 +12,39 @@ def getInput():
 def displayFabric(fabric):
     print('\n'.join([''.join(['{}'.format(item) for item in row]) for row in fabric]))
 
-def starOne(input):
+def processClaims(input):
     maxWidth =  max([int(x['x']) for x in input]) + max([int(x['width']) for x in input])
     maxHeight =  max([int(x['y']) for x in input]) + max([int(x['height']) for x in input])
     fabric = [x[:] for x in [[0] * maxWidth] * maxHeight]
-    overlapCount = 0
     for claim in input:
         offset = (int(claim['x']), int(claim['y']))
         for y in range(offset[1], (offset[1] + int(claim['height']))):
             for x in range(offset[0], (offset[0] + int(claim['width']))):
                 if fabric[y][x] != 0:
                     fabric[y][x] = 'X'
-                    overlapCount += 1
                 else:
                     fabric[y][x] = int(claim['id'])
-    displayFabric(fabric)
+    return fabric
+
+def starOne(fabric):
     xCount = collections.Counter([x for row in fabric for x in row])['X']
     return xCount
         
-def starTwo(input):
-    pass
+def starTwo(input, fabric):
+    cleanFabric = []
+    for claim in input:
+        clean = True
+        offset = (int(claim['x']), int(claim['y']))
+        for y in range(offset[1], (offset[1] + int(claim['height']))):
+            for x in range(offset[0], (offset[0] + int(claim['width']))):
+                if fabric[y][x] == 'X':
+                    clean = False
+        if clean:
+            cleanFabric.append(int(claim['id']))
+    return cleanFabric
 
 if __name__ == "__main__":
     input = getInput()
-    print(starOne(input))
-    print(starTwo(input))
+    claimedFabric = processClaims(input)
+    print(starOne(claimedFabric))
+    print(starTwo(input, claimedFabric))
