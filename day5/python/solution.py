9be72372051d0267727be899d40df02227386fec
Kenny164 <peter.kinney@gmail.com>
2018-12-16 20:02:04 +0000

    day5 star2 completed

--- day5/python/solution.py
+++ day5/python/solution.py
@@ -6,15 +6,15 @@ def getInput():
         return f.readlines()[0]
 
 def reducePolymer(units: List[str]):
-    stats = Counter()
+    stats = set()
     out = list()
     for u in units:
-        stats[u.upper()] += 1
+        stats.add(u.upper())
         if out and u.upper() == out[-1].upper() and u != out[-1]:
             out.pop()
         else:
             out.append(u)
-    return out, stats.most_common(1)[0][0]
+    return out, stats
 
 def removeStaticUnit(polymer: List[str], letter: str):
     out = list()
@@ -28,18 +28,21 @@ def starOne(polymer: List[str]):
     return result
 
 def starTwo(starOneResults: Tuple):
-    reducedPolymer, commonLetter = starOneResults
-    improvedPolymer = removeStaticUnit(reducedPolymer, commonLetter)
-    result, _ = reducePolymer(improvedPolymer)
-    return result
+    stats = Counter()
+    reducedPolymer, letterList = starOneResults
+    for letter in letterList:
+        improvedPolymer = removeStaticUnit(reducedPolymer, letter)
+        result, _ = reducePolymer(improvedPolymer)
+        stats[letter] = len(result)
+    return result, stats.most_common()[-1]
 
 if __name__ == "__main__":
-    # polymerStr = getInput()
-    # starOneOut = starOne(list(getInput()))
-    # print(len(starOneOut[0]))
-    # starTwoOut = starTwo(starOneOut)
-    # print(len(starTwoOut))
-    assert ''.join(starTwo(('dabAcCaCBAcCcaDA','a'))) == 'dbCBcD'
-    assert ''.join(starTwo(('dabAcCaCBAcCcaDA','b'))) == 'daCAcaDA'
-    assert ''.join(starTwo(('dabAcCaCBAcCcaDA','c'))) == 'daDA'
-    assert ''.join(starTwo(('dabAcCaCBAcCcaDA','d'))) == 'abCBAc'
+    polymerStr = getInput()
+    starOneOut = starOne(list(getInput()))
+    print(len(starOneOut[0]))
+    starTwoOut = starTwo(starOneOut)
+    print(starTwoOut[1])
+    # assert ''.join(starTwo(('dabAcCaCBAcCcaDA','a'))) == 'dbCBcD'
+    # assert ''.join(starTwo(('dabAcCaCBAcCcaDA','b'))) == 'daCAcaDA'
+    # assert ''.join(starTwo(('dabAcCaCBAcCcaDA','c'))) == 'daDA'
+    # assert ''.join(starTwo(('dabAcCaCBAcCcaDA','d'))) == 'abCBAc'
