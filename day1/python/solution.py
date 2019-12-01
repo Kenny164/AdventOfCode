e88408585fb5e42f4dc2dd4ef74243a166edfd33
Kenny164 <peter.kinney@gmail.com>
2019-12-01 17:12:42 +0000

    created py3 venv

--- day1/python/solution.py
+++ day1/python/solution.py
@@ -4,11 +4,14 @@ def starOne():
         rollingTotal += int(int(line) / 3) - 2 
     return rollingTotal
 
-def calcFuelRec(mass, roll):
+def calcFuelRec(mass):
     fuel = int(int(mass) / 3) - 2
     if fuel < 1:
         return 0
-    return calcFuel(fuel, roll)
+    return fuel + calcFuelRec(fuel)
+
+assert calcFuelRec(14) == 2
+assert calcFuelRec(1969) == 966
 
 def calcFuel(mass):
     rollingTotal = 0
@@ -27,10 +30,16 @@ assert calcFuel(1969) == 966
 def starTwo():
     rollingTotal = 0
     for line in open("./inputs/day1.txt","r"):
-        #rollingTotal += calcFuelRec(int(line), 0)
         rollingTotal += calcFuel(int(line))
     return rollingTotal
 
+def starTwoRec():
+    rollingTotal = 0
+    for line in open("./inputs/day1.txt","r"):
+        rollingTotal += calcFuelRec(int(line))
+    return rollingTotal
+
 if __name__ == "__main__":
-    print(starTwo())
-    pass
+    print(f'Part 1:    \t{starOne()}')
+    print(f'Part 2:    \t{starTwo()}')
+    print(f'Part 2 rec:\t{starTwoRec()}')
