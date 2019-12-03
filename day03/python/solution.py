ac817601d8798fde66008ba0dc911954e3615a85
Kenny164 <peter.kinney@gmail.com>
2019-12-03 21:16:29 +0000

    day3 part2

--- day03/python/solution.py
+++ day03/python/solution.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 TEST1 = '''R8,U5,L5,D3
 U7,R6,D4,L4'''
 
@@ -25,21 +27,50 @@ def getPathSet(inp: str) -> set:
             p.add(c)
     return p
 
-def getClosestIntersect(inp: str) -> tuple:
+def getSetIntersections(inp: str) -> set:
     sets = [getPathSet(x) for x in inp.splitlines()]
-    intersects = sets[0].intersection(sets[1])
-    return min(intersects, key=lambda x: abs(x[0]) + abs(x[1]))
+    return sets[0].intersection(sets[1])
+
+def getClosestIntersect(inp: set) -> tuple:
+    return min(inp, key=lambda x: abs(x[0]) + abs(x[1]))
 
 def findManhattenDist(x: int, y: int, p: tuple) -> int:
     return abs(x - p[0]) + abs(y - p[1])
 
-assert getClosestIntersect(TEST1) == (3, 3)
-assert getClosestIntersect(TEST2) == (155, 4)
-assert getClosestIntersect(TEST3) == (124, 11)
-assert findManhattenDist(0,0,getClosestIntersect(TEST2)) == 159
+assert getClosestIntersect(getSetIntersections(TEST1)) == (3, 3)
+assert getClosestIntersect(getSetIntersections(TEST2)) == (155, 4)
+assert getClosestIntersect(getSetIntersections(TEST3)) == (124, 11)
+assert findManhattenDist(0,0,getClosestIntersect(getSetIntersections(TEST2))) == 159
 
 with open('./inputs/day03.txt') as f:
-    p1 = getClosestIntersect(f.read())
+    p1 = f.read()
 
-print(p1, findManhattenDist(0, 0, p1))
+wire_intersects = getSetIntersections(p1)
+closest_intersect = getClosestIntersect(wire_intersects)
 
+print('Part1: shortest path Manhatten style:')
+print(closest_intersect, findManhattenDist(0, 0, closest_intersect))
+
+# Part 2:
+TEST4 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83'
+
+def traceIntersects(path: str, intersects: set) -> list:
+    c = (0, 0)
+    step = 0
+    out = []
+    for inst in path.split(','):
+        for _ in range(int(inst[1:])):
+            d = direction[inst[0].upper()]
+            c = d[0] + c[0], d[1] + c[1]
+            step += 1
+            if c in intersects:
+                out.append( (step, c) )
+    return out
+
+cnt = Counter()
+t_ints = getSetIntersections(p1)
+for wire in p1.split('\n'):
+    for x in traceIntersects(wire, t_ints):
+        cnt[(x[1])]+=x[0]
+
+print(f'Part2: shortest path along wires:\n{cnt.most_common()[-1]}')
